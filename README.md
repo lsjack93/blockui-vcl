145e5fb2760617f5eee991a7910dc3d1337fae23
viniciussanchez <viniciuss.sanchez@gmail.com>
2019-05-16 10:55:07 -0300

    Initial Features

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# blockui-vcl
+# Block User Interface for VCL Projects (Delphi)
+This package allows you to lock and unlock the user interface in a simple way.
+
+### Prerequisites
+ * [**Boss**](https://github.com/HashLoad/boss) - Dependency Manager for Delphi
+ 
+### Installation: 
+```
+boss install github.com/viniciussanchez/blockui-vcl
+```
---
+++ src/VCL.BlockUI.pas
@@ -0,0 +1,77 @@
+unit VCL.BlockUI;
+
+interface
+
+uses VCL.BlockUI.Intf, Vcl.Controls, Vcl.Forms;
+
+type
+  TBlockUI = class(TInterfacedObject, IBlockUI)
+  private
+    FLockedForm: TForm;
+    FLockedComponent: TWinControl;
+    procedure Block();
+    procedure Unlock();
+  public
+    constructor Create; overload;
+    constructor Create(const Component: TWinControl); overload;
+    destructor Destroy; override;
+  end;
+
+implementation
+
+uses Vcl.Graphics, System.SysUtils;
+
+procedure TBlockUI.Block;
+begin
+  if Assigned(FLockedForm) then
+    Exit;
+  FLockedForm := TForm.Create(nil);
+  FLockedForm.Position := poDesigned;
+  FLockedForm.BorderStyle := bsNone;
+  FLockedForm.AlphaBlendValue := 110;
+  FLockedForm.AlphaBlend := True;
+  FLockedForm.Color := clBlack;
+  if FLockedComponent = Application.MainForm then
+  begin
+    FLockedForm.Left := FLockedComponent.Left;
+    FLockedForm.Top := FLockedComponent.Top;
+    FLockedForm.Height := FLockedComponent.Height;
+    FLockedForm.Width := FLockedComponent.Width;
+  end
+  else
+  begin
+    FLockedForm.Left := FLockedComponent.ClientOrigin.X;
+    FLockedForm.Top := FLockedComponent.ClientOrigin.Y;
+    FLockedForm.Height := FLockedComponent.ClientHeight;
+    FLockedForm.Width := FLockedComponent.ClientWidth;
+  end;
+  FLockedForm.Show;
+end;
+
+constructor TBlockUI.Create(const Component: TWinControl);
+begin
+  FLockedComponent := Application.MainForm;
+  if Assigned(Component) then
+    FLockedComponent := Component;
+  Self.Block;
+end;
+
+constructor TBlockUI.Create;
+begin
+  Create(nil);
+end;
+
+procedure TBlockUI.Unlock;
+begin
+  if Assigned(FLockedForm) then
+    FreeAndNil(FLockedForm);
+end;
+
+destructor TBlockUI.Destroy;
+begin
+  Self.Unlock;
+  FLockedComponent := nil;
+  inherited;
+end;
+
+end.
