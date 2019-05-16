02b9905ffcd0f66a34f43565c2da1c2544d388a0
viniciussanchez <viniciuss.sanchez@gmail.com>
2019-05-16 13:57:14 -0300

    Bug fixes

--- src/Vcl.BlockUIs.Intf.pas
+++ src/Vcl.BlockUIs.Intf.pas
@@ -5,8 +5,8 @@ interface
 type
   IBlockUI = interface
     ['{2A06489D-4E60-4A98-8FB8-256650ED10E2}']
-    procedure Block();
-    procedure Unlock();
+    procedure Block;
+    procedure Unlock;
   end;
 
 implementation
--- src/Vcl.BlockUIs.pas
+++ src/Vcl.BlockUIs.pas
@@ -9,8 +9,10 @@ type
   private
     FLockedForm: TForm;
     FLockedComponent: TWinControl;
-    procedure Block();
-    procedure Unlock();
+    procedure Block;
+    procedure Unlock;
+  private
+    procedure SetSizeLockedForm;	
   public
     constructor Create; overload;
     constructor Create(const Component: TWinControl); overload;
@@ -23,7 +25,7 @@ uses Vcl.Graphics, System.SysUtils;
 
 procedure TBlockUI.Block;
 begin
-  if Assigned(FLockedForm) then
+  if Assigned(FLockedForm) or (not Assigned(FLockedComponent)) then
     Exit;
   FLockedForm := TForm.Create(nil);
   FLockedForm.Position := poDesigned;
@@ -31,28 +33,16 @@ begin
   FLockedForm.AlphaBlendValue := 110;
   FLockedForm.AlphaBlend := True;
   FLockedForm.Color := clBlack;
-  if FLockedComponent = Application.MainForm then
-  begin
-    FLockedForm.Left := FLockedComponent.Left;
-    FLockedForm.Top := FLockedComponent.Top;
-    FLockedForm.Height := FLockedComponent.Height;
-    FLockedForm.Width := FLockedComponent.Width;
-  end
-  else
-  begin
-    FLockedForm.Left := FLockedComponent.ClientOrigin.X;
-    FLockedForm.Top := FLockedComponent.ClientOrigin.Y;
-    FLockedForm.Height := FLockedComponent.ClientHeight;
-    FLockedForm.Width := FLockedComponent.ClientWidth;
-  end;
+  SetSizeLockedForm;
   FLockedForm.Show;
 end;
 
 constructor TBlockUI.Create(const Component: TWinControl);
 begin
-  FLockedComponent := Application.MainForm;
   if Assigned(Component) then
-    FLockedComponent := Component;
+    FLockedComponent := Component
+  else if Assigned(Application.MainForm) then
+    FLockedComponent := Application.MainForm;
   Self.Block;
 end;
 
@@ -72,6 +62,24 @@ begin
   Self.Unlock;
   FLockedComponent := nil;
   inherited;
+end;
+
+procedure TBlockUI.SetSizeLockedForm;
+begin
+  if FLockedComponent = Application.MainForm then
+  begin
+    FLockedForm.Left := FLockedComponent.Left;
+    FLockedForm.Top := FLockedComponent.Top;
+    FLockedForm.Height := FLockedComponent.Height;
+    FLockedForm.Width := FLockedComponent.Width;
+  end
+  else
+  begin
+    FLockedForm.Left := FLockedComponent.ClientOrigin.X;
+    FLockedForm.Top := FLockedComponent.ClientOrigin.Y;
+    FLockedForm.Height := FLockedComponent.ClientHeight;
+    FLockedForm.Width := FLockedComponent.ClientWidth;
+  end;
 end;
 
 end.
